a1b90f679ae821bfab825085555fe0aa80a64cd4
Mohamed MotYim <mohamed.motyim@gmail.com>
2019-02-16 14:43:49 +0200

    Enhance UI remove metadate

--- src/main/java/me/motyim/freelance/ersystem/config/SwaggerConfig.java
+++ src/main/java/me/motyim/freelance/ersystem/config/SwaggerConfig.java
@@ -2,11 +2,8 @@ package me.motyim.freelance.ersystem.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -24,22 +21,9 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(metaDate())                                        //to add api meta data
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
                 .paths(PathSelectors.any())
-                .build();
-    }
-
-
-    private ApiInfo metaDate() {
-        return new ApiInfoBuilder()
-                .title("ersystem")
-                .description("Demo project for Environmental Reporter System")
-                .version("1.0.0")
-                .license("Apache License Version 2.0")
-                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
-                .contact(new Contact("Motyim","http://www.motyim.me","mohamed.motyim@gmail.com"))
                 .build();
     }
 
