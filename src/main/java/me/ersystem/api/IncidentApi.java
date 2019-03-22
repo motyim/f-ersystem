b597d207e63b2e45933f7aaacaaf14c5d6151718
Mohamed MotYim <motyim@users.noreply.github.com>
2019-03-22 14:13:04 +0200

    Fix - incident API Response

--- src/main/java/me/ersystem/api/IncidentApi.java
+++ src/main/java/me/ersystem/api/IncidentApi.java
@@ -5,6 +5,7 @@ import me.ersystem.dto.IncidentResponse;
 import me.ersystem.dto.UploadImageDto;
 import me.ersystem.service.IncidentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,24 +24,22 @@ public class IncidentApi {
 
 
     @PostMapping()
-    public ResponseEntity<Integer> addIncident(@RequestBody IncidentDto dto) {
+    public ResponseEntity<String> addIncident(@RequestBody IncidentDto dto) {
         Integer integer = service.addIncident(dto);
-        return ResponseEntity.ok(integer);
+        return ResponseEntity.ok("{\"id\":"+integer+"}");
     }
 
     @PostMapping("upload")
-    public ResponseEntity<String> uploadImage(@RequestBody UploadImageDto imageDto){
+    public ResponseEntity<HttpStatus> uploadImage(@RequestBody UploadImageDto imageDto){
         service.uploadImage(imageDto);
-        return ResponseEntity.ok("Uploaded");
+        return new ResponseEntity( HttpStatus.OK );
     }
 
     @GetMapping
     public ResponseEntity<List<IncidentResponse>> getAll(int id) {
-
         List<IncidentResponse> allIncident = service.getAllIncidentByUserId(id);
         System.out.println(allIncident.size());
         return ResponseEntity.ok(allIncident);
-
     }
 
 }
